c77c7ce8bc75d6045eeb660a2b054cbacce2fb8b
nakitmn <glembockiy98@gmail.com>
2025-02-28 00:03:09 +0300

    victory zone

---
+++ CodHomework/Assets/Game/Scripts/Gameplay/Entities/VictoryZone/VictoryZone.cs
@@ -0,0 +1,33 @@
+﻿using Modules.Entity;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public sealed class VictoryZone : MonoEntity
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<TriggerEntityDetectorComponent>()
+                .FromComponentInHierarchy()
+                .AsSingle()
+                .NonLazy();
+            
+            Container.Bind<AudioSource>()
+                .FromComponentInHierarchy()
+                .AsSingle();
+        }
+
+        public override void Start()
+        {
+            var entityDetectorComponent = Get<TriggerEntityDetectorComponent>();
+            entityDetectorComponent.OnDetected += entity =>
+            {
+                if (entity is Character)
+                {
+                    Get<AudioSource>().Play();
+                    entityDetectorComponent.enabled = false;
+                }
+            };
+        }
+    }
+}
