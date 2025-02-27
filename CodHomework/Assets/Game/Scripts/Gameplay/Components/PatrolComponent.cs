954cc36d3b9a4a47ec88a8a502e6f260f3cc9e9c
nakitmn <glembockiy98@gmail.com>
2025-02-27 20:51:34 +0300

    moving platforms

---
+++ CodHomework/Assets/Game/Scripts/Gameplay/Components/PatrolComponent.cs
@@ -0,0 +1,32 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public sealed class PatrolComponent : MonoBehaviour
+    {
+        [SerializeField] private PatrolPointsComponent _pointsComponent;
+        [SerializeField] private float _speed;
+
+        private void Start()
+        {
+            MoveToCurrentPoint();
+        }
+
+        private void MoveToCurrentPoint()
+        {
+            transform.DOMove(_pointsComponent.Current.position, _speed)
+                .SetSpeedBased(true)
+                .SetEase(Ease.Linear)
+                .SetLink(gameObject)
+                .SetUpdate(UpdateType.Fixed)
+                .OnComplete(OnDestinated);
+        }
+
+        private void OnDestinated()
+        {
+            _pointsComponent.Next();
+            MoveToCurrentPoint();
+        }
+    }
+}
