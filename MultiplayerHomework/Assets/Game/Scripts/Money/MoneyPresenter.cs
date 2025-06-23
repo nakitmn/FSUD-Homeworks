0aaf059eb7a01c5b1ea1a8faca5e97a0cef074b8
nakitmn <glembockiy98@gmail.com>
2025-06-23 09:47:30 +0300

    money presenter refactored

--- MultiplayerHomework/Assets/Game/Scripts/Money/MoneyPresenter.cs
+++ MultiplayerHomework/Assets/Game/Scripts/Money/MoneyPresenter.cs
@@ -1,13 +1,21 @@
 ﻿using TMPro;
 using UnityEngine;
+using Zenject;
 
 namespace Game
 {
     public sealed class MoneyPresenter : MonoBehaviour
     {
-        [SerializeField] private MoneyStorage _moneyStorage;
         [SerializeField] private TMP_Text _amount;
+        
+        private MoneyStorage _moneyStorage;
 
+        [Inject]
+        public void Construct(MoneyStorage moneyStorage)
+        {
+            _moneyStorage = moneyStorage;
+        }
+        
         private void OnEnable()
         {
             _moneyStorage.OnMoneyChanged += OnMoneyChanged;
