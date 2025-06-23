b12d6197e340068fbbe095eb3f78cbce9553f971
nakitmn <glembockiy98@gmail.com>
2025-06-23 12:25:28 +0300

    camera refactored

--- MultiplayerHomework/Assets/Game/Scripts/Camera/CameraController.cs
+++ MultiplayerHomework/Assets/Game/Scripts/Camera/CameraController.cs
@@ -1,27 +1,26 @@
-﻿using UnityEngine;
+﻿using Fusion;
+using UnityEngine;
 
 namespace Game
 {
-    public sealed class CameraController : MonoBehaviour
+    public sealed class CameraController : SimulationBehaviour
     {
         [SerializeField] private Transform _follower;
         [SerializeField] private float _speed;
-        
-        private Transform _target;
 
-        public void SetTarget(Transform target)
+        public override void Render()
         {
-            _target = target;
-        }
-
-        private void LateUpdate()
-        {
-            if (_target == null)
+            var playerObject = Runner.GetPlayerObject(Runner.LocalPlayer);
+            if (playerObject == null)
             {
                 return;
             }
 
-            _follower.position = Vector3.Lerp(_follower.position, _target.position, _speed * Time.deltaTime);
+            _follower.position = Vector3.Lerp(
+                _follower.position,
+                playerObject.transform.position,
+                _speed * Time.deltaTime
+            );
         }
     }
 }
