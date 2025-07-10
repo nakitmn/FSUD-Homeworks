8afd70007abcfc0a2aa4429f7c927a1a6b3685af
nakitmn <glembockiy98@gmail.com>
2025-07-10 11:58:08 +0300

    smooth health bar

--- AtomicProjectWork/Assets/Game/Scripts/UI/Health/HealthView.cs
+++ AtomicProjectWork/Assets/Game/Scripts/UI/Health/HealthView.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,14 +7,13 @@ namespace SampleGame
 {
     public sealed class HealthView : MonoBehaviour
     {
-        [SerializeField]
-        private TMP_Text _text;
-
-        [SerializeField]
-        private Image[] _fillImages;
-
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private Image[] _fillImages;
         [SerializeField] private RectTransform _border;
-        
+        [SerializeField] private float _smoothDuration = 0.5f;
+
+        private Sequence _sequence;
+
         public void SetValue(string text)
         {
             _text.text = text;
@@ -21,16 +21,31 @@ namespace SampleGame
 
         public void SetProgress(float progress)
         {
-            var anchorMin = _border.anchorMin;
-            anchorMin.y = progress;
-            var anchorMax = _border.anchorMax;
-            anchorMax.y = progress;
-            _border.anchorMin = anchorMin;
-            _border.anchorMax = anchorMax;
-            
+            if (_sequence.IsActive())
+            {
+                _sequence.Kill();
+            }
+
+            _sequence = DOTween.Sequence();
+            _sequence.SetLink(gameObject);
+
+            _sequence.Join(
+                DOVirtual.Float(_border.anchorMin.y, progress, _smoothDuration / 2f, value =>
+                {
+                    var anchorMin = _border.anchorMin;
+                    anchorMin.y = value;
+                    var anchorMax = _border.anchorMax;
+                    anchorMax.y = value;
+                    _border.anchorMin = anchorMin;
+                    _border.anchorMax = anchorMax;
+                })
+            );
+
             foreach (var fillImage in _fillImages)
             {
-                fillImage.fillAmount = progress;
+                _sequence.Join(
+                    fillImage.DOFillAmount(progress, _smoothDuration)
+                );
             }
         }
     }
