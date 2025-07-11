8ac4df88e086833f6621a6d6547476840537326c
nakitmn <glembockiy98@gmail.com>
2025-07-11 13:13:47 +0300

    destroyable props visual

--- AtomicProjectWork/Assets/Game/Scripts/GameEntity/Content/DestroyableProps/DestroyablePropInstaller.cs
+++ AtomicProjectWork/Assets/Game/Scripts/GameEntity/Content/DestroyableProps/DestroyablePropInstaller.cs
@@ -3,23 +3,20 @@ using UnityEngine;
 
 namespace SampleGame
 {
-    public class DestroyablePropInstaller : SceneEntityInstaller<IGameEntity>
+    public sealed class DestroyablePropInstaller : SceneEntityInstaller<IGameEntity>
     {
         [SerializeField] private LifeInstaller _lifeInstaller;
         [SerializeField] private LootInstaller _lootInstaller;
-        
+
         protected override void Install(IGameEntity entity)
         {
             entity.AddGameObject(gameObject);
             entity.AddTransform(transform);
-            
+
             _lifeInstaller.Install(entity);
             _lootInstaller.Install(entity);
-            
-            entity.GetDeathEvent().Subscribe(() =>
-            {
-                gameObject.SetActive(false);
-            });
+
+            entity.GetDeathEvent().Subscribe(() => { gameObject.SetActive(false); });
         }
     }
 }
---
+++ AtomicProjectWork/Assets/Game/Scripts/GameEntity/Content/DestroyableProps/DestroyablePropVisualInstaller.cs
@@ -0,0 +1,29 @@
+﻿using Atomic.Entities;
+using UnityEngine;
+
+namespace SampleGame
+{
+    public sealed class DestroyablePropVisualInstaller : SceneEntityInstaller<IGameEntity>
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string _damagedAnimationKey;
+        [SerializeField] private ParticleSystem _damagedVfx;
+        [SerializeField] private GameObject _deathVfx;
+        [SerializeField] private Transform _deathVfxPoint;
+
+        protected override void Install(IGameEntity entity)
+        {
+            var gameContext = GameContext.Instance;
+
+            entity.AddAnimator(_animator);
+
+            entity.AddBehaviour(new EventVfxBehaviour(entity.GetDamagedEvent(), _damagedVfx));
+            entity.AddBehaviour(new TakeDamageAnimBehaviour(_damagedAnimationKey));
+            
+            entity.GetDeathEvent().Subscribe(() =>
+            {
+                gameContext.GetPrefabPool().Rent(_deathVfx, _deathVfxPoint.position, _deathVfxPoint.rotation);
+            });
+        }
+    }
+}
