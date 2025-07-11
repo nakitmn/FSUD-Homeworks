d47ee784d1c70c20b26b0ed794430fdd11741a80
nakitmn <glembockiy98@gmail.com>
2025-07-11 09:46:22 +0300

    ParticlePoolReturner fix

--- AtomicProjectWork/Assets/Game/Scripts/Pool/ParticlePoolReturner.cs
+++ AtomicProjectWork/Assets/Game/Scripts/Pool/ParticlePoolReturner.cs
@@ -2,29 +2,17 @@
 
 namespace SampleGame
 {
-    [RequireComponent(typeof(ParticlesEventReceiver))]
+    [RequireComponent(typeof(ParticleSystem))]
     public sealed class ParticlePoolReturner : MonoBehaviour
     {
-        private ParticlesEventReceiver _particlesEventReceiver;
         private GameContext _gameContext;
 
         private void Awake()
         {
-            _particlesEventReceiver = GetComponent<ParticlesEventReceiver>();
             _gameContext = GameContext.Instance;
         }
 
-        private void OnEnable()
-        {
-            _particlesEventReceiver.OnStopped += ReturnToPool;
-        }
-
-        private void OnDisable()
-        {
-            _particlesEventReceiver.OnStopped -= ReturnToPool;
-        }
-
-        private void ReturnToPool()
+        private void OnParticleSystemStopped()
         {
             _gameContext.GetPrefabPool().Return(gameObject);
         }
