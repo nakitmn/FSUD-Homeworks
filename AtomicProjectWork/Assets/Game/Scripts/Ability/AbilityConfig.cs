bef2ce35d29d756f494c45c7e33caec34b7d8317
nakitmn <glembockiy98@gmail.com>
2025-07-11 18:06:24 +0300

    removed ability description

--- AtomicProjectWork/Assets/Game/Scripts/Ability/AbilityConfig.cs
+++ AtomicProjectWork/Assets/Game/Scripts/Ability/AbilityConfig.cs
@@ -10,9 +10,6 @@ namespace SampleGame
         public string Name { get; private set; }
 
         [field: SerializeField]
-        public string Description { get; private set; }
-
-        [field: SerializeField]
         public Sprite Icon { get; private set; }
 
         [field: SerializeField]
