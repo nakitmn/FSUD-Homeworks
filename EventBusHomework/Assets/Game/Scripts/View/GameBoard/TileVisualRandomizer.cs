f417f383f35c08ed8b8dfc3b3b8b4a6dac6cd800
nakitmn <glembockiy98@gmail.com>
2025-08-07 22:03:12 +0300

    environment demo visual

---
+++ EventBusHomework/Assets/Game/Scripts/View/GameBoard/TileVisualRandomizer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace SampleGame
+{
+    public sealed class TileVisualRandomizer : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _bottomDecors;
+        [SerializeField] private GameObject[] _topDecors;
+        
+        private void Start()
+        {
+            Randomize();
+        }
+
+        public void Randomize()
+        {
+            foreach (var decor in _bottomDecors)
+            {
+                decor.SetActive(Random.value < 0.5f);
+            }
+
+            foreach (var decor in _topDecors)
+            {
+                decor.SetActive(Random.value < 0.5f);
+            }
+        }
+    }
+}
