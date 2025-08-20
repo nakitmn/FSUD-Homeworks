5ed0a6fe0c2b5b46126b312942b098b88d6120d4
nakitmn <glembockiy98@gmail.com>
2025-08-20 14:30:36 +0300

    end turn button hide logic

---
+++ EventBusHomework/Assets/Game/Scripts/View/Commands/Content/CallbackAnimationCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace SampleGame
+{
+    public readonly struct CallbackAnimationCommand : IAnimationCommand
+    {
+        private readonly Action _callback;
+
+        public CallbackAnimationCommand(Action callback)
+        {
+            _callback = callback;
+        }
+
+        public async UniTask Execute()
+        {
+            _callback?.Invoke();
+            await UniTask.CompletedTask;
+        }
+    }
+}
--- EventBusHomework/Assets/Game/Scripts/View/UI/EndTurnButtonPresenter.cs
+++ EventBusHomework/Assets/Game/Scripts/View/UI/EndTurnButtonPresenter.cs
@@ -7,9 +7,10 @@ namespace SampleGame
 {
     public sealed class EndTurnButtonPresenter : IEnable<IViewContext>, IDisable
     {
-         private readonly Button _button;
+        private readonly Button _button;
 
         private IEventBus _eventBus;
+        private IViewContext _viewContext;
 
         public EndTurnButtonPresenter(Button button)
         {
@@ -19,19 +20,36 @@ namespace SampleGame
         public void Enable(IViewContext entity)
         {
             var gameContext = GameContext.Instance;
+            _viewContext = entity;
             _eventBus = gameContext.GetEventBus();
-            
+
+            _eventBus.SubscribeStartPlayerTurn(OnPlayerTurnStarted);
+            _eventBus.SubscribeEndPlayerTurn(OnPlayerTurnEnded);
             _button.onClick.AddListener(OnClicked);
         }
 
         public void Disable(in IEntity entity)
         {
+            _eventBus.UnsubscribeStartPlayerTurn(OnPlayerTurnStarted);
+            _eventBus.UnsubscribeEndPlayerTurn(OnPlayerTurnEnded);
             _button.onClick.RemoveListener(OnClicked);
         }
 
         private void OnClicked()
         {
             _eventBus.InvokeEndPlayerTurn();
+        }
+
+        private void OnPlayerTurnEnded()
+        {
+            _button.gameObject.SetActive(false);
+        }
+
+        private void OnPlayerTurnStarted()
+        {
+            ViewCommandsUseCase.Enqueue(_viewContext,
+                new CallbackAnimationCommand(() => _button.gameObject.SetActive(true))
+            );
         }
     }
 }
